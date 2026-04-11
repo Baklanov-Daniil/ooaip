e4dfa6d31302f7372edfb0c2760ab92c0ca3790b
Daniil <daniilbaklanov55@gmail.com>
2026-04-11 12:09:34 +0600

    final

--- SpaceBattle.Lib/SendCommand.cs
+++ SpaceBattle.Lib/SendCommand.cs
@@ -2,8 +2,13 @@ namespace SpaceBattle.Lib;
 
 public class SendCommand : ICommand
 {
+<<<<<<< HEAD
     private ICommand _command { get; }
     private ICommandReceiver _receiver { get; }
+=======
+    private ICommand _command {get;}
+    private ICommandReceiver _receiver {get;}
+>>>>>>> 0fe2ab7 (final)
     public SendCommand(ICommand command, ICommandReceiver receiver)
     {
         _command = command;
