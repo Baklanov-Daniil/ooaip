baca9f9a418fe1782393d9e31c56c2300a97eb92
Belogrivtsev <nuclearthrone55@gmail.com>
2026-05-23 12:40:27 +0600

    feat: RegisterIoCDependencyRotateCommand and tests for it

---
+++ SpaceBattle.Tests/RegisterIoCDependencyRotateCommandTests.cs
@@ -0,0 +1,40 @@
+using App;
+using App.Scopes;
+using Moq;
+
+using SpaceBattle.Lib;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class RegisterIoCDependencyRotateCommandTests : IDisposable
+    {
+        public RegisterIoCDependencyRotateCommandTests()
+        {
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+        }
+
+        [Fact]
+        public void Execute_ShouldRegisterRotateCommandDependency()
+        {
+            var mockAdapter = new Mock<IDictionary<string, object>>();
+            var mockGameObject = new Mock<object>();
+            
+            Ioc.Resolve<ICommand>("IoC.Register", "Adapters.IRotatingObject", (object[] args)
+             => mockAdapter.Object).Execute();
+
+            var register = new RegisterIoCDependencyRotateCommand();
+            register.Execute();
+
+            var command = Ioc.Resolve<ICommand>("Commands.Rotate", mockGameObject.Object);
+            
+            Assert.NotNull(command); 
+        }
+
+        public void Dispose()
+        {
+            Ioc.Resolve<ICommand>("IoC.Scope.Current.Clear").Execute();
+        }
+    }
+}
