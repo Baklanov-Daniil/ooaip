ede36fcf8dc714adabbfd1c59bd2f09397285d8d
dexffe <dexffe@gmail.com>
2026-05-27 16:58:11 +0600

    19. fix(Ioc)

--- SpaceBattle.Lib/RegisterIoCDependencyActionsStart.cs
+++ SpaceBattle.Lib/RegisterIoCDependencyActionsStart.cs
@@ -3,34 +3,48 @@ using App;
 
 namespace SpaceBattle.Lib;
 
+public class StartCommand : App.ICommand
+{
+    private readonly IDictionary<string, object> _order;
+
+    public StartCommand(IDictionary<string, object> order)
+    {
+        _order = order;
+    }
+
+    public void Execute()
+    {
+        var queue = (BlockingCollection<App.ICommand>)_order["Queue"];
+
+        var thread = new Thread(() =>
+        {
+            foreach (var cmd in queue.GetConsumingEnumerable())
+            {
+                cmd.Execute();
+            }
+        })
+        { IsBackground = true };
+
+        _order["Thread"] = thread;
+        thread.Start();
+    }
+}
+
 public class RegisterIoCDependencyActionsStart : App.ICommand
 {
     public void Execute()
     {
-        try
-        {
-            Ioc.Resolve<object>("Actions.Start");
-        }
-        catch
-        {
-            Ioc.Resolve<App.ICommand>(
-                "IoC.Register", 
-                "Actions.Start", 
-                (object[] args) => new StartCommand((IDictionary<string, object>)args[0])
-            ).Execute();
-        }
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register", 
+            "Actions.Start", 
+            (object[] args) => new StartCommand((IDictionary<string, object>)args[0])
+        ).Execute();
 
-        try
-        {
-            Ioc.Resolve<object>("Actions.Queue.Create");
-        }
-        catch
-        {
-            Ioc.Resolve<App.ICommand>(
-                "IoC.Register", 
-                "Actions.Queue.Create", 
-                (object[] args) => new BlockingCollection<App.ICommand>()
-            ).Execute();
-        }
+
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register", 
+            "Actions.Queue.Create", 
+            (object[] args) => new BlockingCollection<App.ICommand>()
+        ).Execute();
     }
 }
--- SpaceBattle.Tests/RegisterIoCDependencyActionsStartTests.cs
+++ SpaceBattle.Tests/RegisterIoCDependencyActionsStartTests.cs
@@ -5,9 +5,9 @@ using SpaceBattle.Lib;
 
 namespace SpaceBattle.Tests;
 
-public class RegisterIoCDependencyActionsStartTests : IDisposable
+public class RegisterIoCDependencyActionsStartTests_19task : IDisposable
 {
-    public RegisterIoCDependencyActionsStartTests()
+    public RegisterIoCDependencyActionsStartTests_19task()
     {
         new App.Scopes.InitCommand().Execute();
         Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Clear").Execute();
@@ -29,6 +29,31 @@ public class RegisterIoCDependencyActionsStartTests : IDisposable
         };
 
         var cmd = Ioc.Resolve<App.ICommand>("Actions.Start", order);
+
         Assert.NotNull(cmd);
+    }
+
+    [Fact]
+    public void StartCommand_Execute_StartsThread()
+    {
+        new RegisterIoCDependencyActionsStart().Execute();
+
+        var queue = new BlockingCollection<App.ICommand>();
+        IDictionary<string, object> order = new Dictionary<string, object>
+        {
+            ["Queue"] = queue
+        };
+
+        var startCmd = Ioc.Resolve<App.ICommand>("Actions.Start", order);
+        
+        startCmd.Execute();
+
+        Assert.True(order.ContainsKey("Thread"));
+        
+        var thread = (Thread)order["Thread"];
+        Assert.NotNull(thread);
+        Assert.True(thread.IsAlive);
+
+        queue.CompleteAdding();
     }
 }
