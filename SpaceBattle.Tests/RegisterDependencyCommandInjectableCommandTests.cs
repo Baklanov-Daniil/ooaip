f70c635e1e870a6be1a1316ba55894f18c5efa94
dexffe <dexffe@gmail.com>
2026-05-27 16:11:00 +0600

    18. fix(Ioc)

---
+++ SpaceBattle.Tests/RegisterDependencyCommandInjectableCommandTests.cs
@@ -0,0 +1,38 @@
+using Xunit;
+using App;
+using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests;
+
+public class RegisterDependencyCommandInjectableCommandTests : IDisposable
+{
+    public RegisterDependencyCommandInjectableCommandTests()
+    {
+        new App.Scopes.InitCommand().Execute();
+        Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Clear").Execute();
+    }
+
+    public void Dispose()
+    {
+        Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Clear").Execute();
+    }
+
+    [Fact]
+    public void AfterExecute_AllThreeResolveTypesWork_WithoutException()
+    {
+        // Arrange
+        var registerCmd = new RegisterDependencyCommandInjectableCommand();
+
+        // Act
+        registerCmd.Execute();
+
+        // Assert
+        var asICommand = Ioc.Resolve<ICommand>("Commands.CommandInjectable");
+        var asICommandInjectable = Ioc.Resolve<ICommandInjectable>("Commands.CommandInjectable");
+        var asConcreteType = Ioc.Resolve<CommandInjectableCommand>("Commands.CommandInjectable");
+
+        Assert.NotNull(asICommand);
+        Assert.NotNull(asICommandInjectable);
+        Assert.NotNull(asConcreteType);
+    }
+}
