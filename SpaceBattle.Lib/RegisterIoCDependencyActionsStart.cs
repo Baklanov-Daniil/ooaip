ce5e4a3baba1432a673fd8d17fb0087b0038f2a1
dexffe <dexffe@gmail.com>
2026-05-27 17:03:57 +0600

    19. fix(Ioc)

--- SpaceBattle.Lib/RegisterIoCDependencyActionsStart.cs
+++ SpaceBattle.Lib/RegisterIoCDependencyActionsStart.cs
@@ -34,30 +34,17 @@ public class RegisterIoCDependencyActionsStart : App.ICommand
 {
     public void Execute()
     {
-        try
-        {
-            Ioc.Resolve<object>("Actions.Start");
-        }
-        catch
-        {
-            Ioc.Resolve<App.ICommand>(
-                "IoC.Register", 
-                "Actions.Start", 
-                (object[] args) => new StartCommand((IDictionary<string, object>)args[0])
-            ).Execute();
-        }
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register", 
+            "Actions.Start", 
+            (object[] args) => new StartCommand((IDictionary<string, object>)args[0])
+        ).Execute();
 
-        try
-        {
-            Ioc.Resolve<object>("Actions.Queue.Create");
-        }
-        catch
-        {
-            Ioc.Resolve<App.ICommand>(
-                "IoC.Register", 
-                "Actions.Queue.Create", 
-                (object[] args) => new BlockingCollection<App.ICommand>()
-            ).Execute();
-        }
+
+        Ioc.Resolve<App.ICommand>(
+            "IoC.Register", 
+            "Actions.Queue.Create", 
+            (object[] args) => new BlockingCollection<App.ICommand>()
+        ).Execute();
     }
 }
