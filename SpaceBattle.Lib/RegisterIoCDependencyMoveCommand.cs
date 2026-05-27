47e63fd70cc2f65bfad2d20cd8e3be4d12caa811
LeeAnna241 <anastasiakisner6@gmail.com>
2026-05-27 22:08:40 +0600

    registerMoveCommandDependency

---
+++ SpaceBattle.Lib/RegisterIoCDependencyMoveCommand.cs
@@ -0,0 +1,20 @@
+using App;
+namespace SpaceBattle.Lib
+{
+    public class RegisterIoCDependencyMoveCommand : ICommand
+    {
+        public void Execute()
+        {
+        var registerMoveCommandDependency = Ioc.Resolve<App.ICommand>(
+            "IoC.Register",
+            "Commands.MoveCommand",
+            (object[] args) => {var movingObject = (IDictionary<string, object>)args[0];
+                                var adapter = Ioc.Resolve<IMoving>("Adapters.IMovingObject", movingObject);
+                                return new MoveCommand(adapter);
+                                }
+        );
+
+        registerMoveCommandDependency.Execute();
+        }
+    }
+}
