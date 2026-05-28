61fce6a98cbc1fdc5a8f0c4f699b0160c94fb086
Baklanov-Daniil <daniilbaklanov55@gmail.com>
2026-05-28 12:10:44 +0600

    add tests

--- SpaceBattle.Tests/RegisterIocGameOperationTests.cs
+++ SpaceBattle.Tests/RegisterIocGameOperationTests.cs
@@ -24,6 +24,28 @@ namespace SpaceBattle.Lib.Tests
         }
 
         [Fact]
+        public void Execute_RegistersCommandsQueue()
+        {
+            var operation = new IoCRegisterGameOperation();
+            operation.Execute();
+
+            var queue = Ioc.Resolve<Queue<SpaceBattle.Lib.ICommand>>("Game.CommandsQueue");
+            Assert.NotNull(queue);
+            Assert.Empty(queue);
+            Assert.IsType<Queue<SpaceBattle.Lib.ICommand>>(queue);
+        }
+
+        [Fact]
+        public void Execute_RegistersTimeQuant()
+        {
+            var operation = new IoCRegisterGameOperation();
+            operation.Execute();
+
+            var timeQuant = Ioc.Resolve<TimeSpan>("Game.TimeQuant");
+            Assert.Equal(TimeSpan.FromMilliseconds(100), timeQuant);
+        }
+
+        [Fact]
         public void Execute_RegistersExceptionHandler()
         {
             var operation = new IoCRegisterGameOperation();
@@ -34,6 +56,19 @@ namespace SpaceBattle.Lib.Tests
             #pragma warning restore CS8625
             Assert.NotNull(handler);
             Assert.IsType<NullCommand>(handler);
+        }
+
+        [Fact]
+        public void Execute_RegistersAllDependencies()
+        {
+            var operation = new IoCRegisterGameOperation();
+            operation.Execute();
+
+            Assert.NotNull(Ioc.Resolve<ICommandReceiver>("Game.Receiver"));
+            Assert.NotNull(Ioc.Resolve<Queue<SpaceBattle.Lib.ICommand>>("Game.CommandsQueue"));
+            #pragma warning disable CS8625
+            Assert.NotNull(Ioc.Resolve<ICommand>("ExceptionHandler.Handle", null, null));
+            #pragma warning restore CS8625
         }
 
         public void Dispose()
